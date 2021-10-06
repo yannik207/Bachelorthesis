e237e6dfb4dfad8f08a209a5b6627ad02f2a89b1
yannik207 <ysacherer@icloud.com>
2021-10-06 14:39:21 +0200

    fixed covid problems

--- Bachelorarbeit_Skript.R
+++ Bachelorarbeit_Skript.R
@@ -10,6 +10,7 @@ library(plyr)
 library(lubridate)
 library(corrplot)
 library(RColorBrewer)
+library(xlsx)
 
 # datan laden
 
@@ -21,6 +22,7 @@ founding_data <- read_csv("fouding_data.csv")
 founding_data <- str_split_fixed(founding_data$`Issuer Name...2;Date of Incorporation`, ";", 2)
 founding.data <- as.data.frame(founding_data)
 colnames(founding_data) <- c("Unternehmensname", "founding_date")
+names(initial_data)[names(initial_data) == "Gross Proceeds Inc. Overallotment Exercised"] <- "IPO_Erlös"
 raw_data <- merge(x = initial_data, y = founding_data, by.x = 'Issuer Name...2', by.y = 'Unternehmensname')
 
 # drop unnecesarry columns
@@ -41,7 +43,7 @@ raw_data$founding_date <- na_if(raw_data$founding_date, "Unable to collect data 
 
 n_samples_with_NA <- nrow(raw_data)
 clean_data <- na.omit(raw_data)
-n_samples_without_NA <- nrow(raw_data_wo_NA)
+n_samples_without_NA <- nrow(clean_data)
 
 round((n_samples_with_NA - n_samples_without_NA) / n_samples_with_NA, 3)
 
@@ -52,7 +54,7 @@ any(is.na(clean_data))
 
 orderd_data <- clean_data[order(clean_data$`Issue Date`),]
 orderd_data$`Issue Date` <-as.Date(as.POSIXct(orderd_data$`Issue Date`, 'GMT'))
-orderd_data$founding_date <- as.Date(as.POSIXct(order_data$founding_date, 'GMT'))
+orderd_data$founding_date <- as.Date(as.POSIXct(orderd_data$founding_date, 'GMT'))
 
 orderd_data$dummyCOVID <- ifelse(orderd_data$`Issue Date` < as.Date("24/02/2020", format = "%d/%m/%Y") &
                               orderd_data$`Issue Date` < as.Date("23/02/2020", format = "%d/%m/%Y"), 1, 0)
@@ -63,25 +65,65 @@ table(orderd_data$dummy)
 orderd_data$founding_date <- as.Date(orderd_data$founding_date)
 orderd_data$age <- round(((orderd_data$`Issue Date`-orderd_data$founding_date) / 365),2)
 
-orderd_data$age[orderd_data$age < 1] <- mean(orderd_data$age)
+# for the age i need a subsample
+subsample <- orderd_data
 
-aged_data <- na.omit(orderd_data)
+subsample$age[subsample$age < 1] <- NA
+
+subsample <- na.omit(subsample)
 
 hightechsic <- c("Software", "Integrated Telecommunications Services", "Communications Equipment", "Computer Hardware", "Electrical Components & Equipment", "Consumer Electronics", "Electric Utilities", "IT Services & Consulting", "Aerospace & Defense", "Biotechnology & Medical Research")
 
 orderd_data$hightech_dummy<- ifelse(orderd_data$`Issuer TRBC Industry` %in% hightechsic , 1, 0)
+subsample$hightech_dummy<- ifelse(subsample$`Issuer TRBC Industry` %in% hightechsic , 1, 0)
+
 
 europe <- c("Germany", "France", "Belgium", "Spain")
 
 
 orderd_data$dummynation <- ifelse(orderd_data$`Issuer Nation` %in% europe, 1,0)
 
-final <- orderd_data[c(2, 10, 13, 14, 15, 16)]
+# section 2 describing statistics
+final <- orderd_data[c(2, 10, 12, 14, 15)]
+# how many companies are in US and Europa
+final %>% group_by(dummynation) %>% summarise(count = count(dummynation))
 
-final$age <- as.numeric(final$age)
+# how is the underpricing difference in europa and usa
+final %>% filter(dummynation == 0) %>% summarise(mean = mean(Underpricing))
+final %>% filter(dummynation == 1) %>% summarise(mean = mean(Underpricing))
+
+
+
+# how is the underpricing during covid-19
+final %>% filter(dummyCOVID == 0) %>% summarise(mean = mean(Underpricing))
+final %>% filter(dummyCOVID == 1) %>% summarise(mean = mean(Underpricing))
+
+# can't find a significant difference between before and during COVID-19
+# therefor I change the covid timeline and just the time between stock drop and stock recovery
+data_w_covid <- orderd_data %>% select(Underpricing, `Issue Date`) %>% filter(`Issue Date` > "2020-02-24", `Issue Date` < "2020-07-30")
+
+data_wo_covid <- orderd_data %>% select(Underpricing, `Issue Date`) %>% filter(`Issue Date` < "2020-02-24" | `Issue Date` > "2020-07-30") 
+
+t.test(data_w_covid$Underpricing, data_wo_covid$Underpricing, alternative = "greater")
+
+# how is the underpricing in hightech industries
+
+final %>% filter(hightech_dummy == 0) %>% summarise(mean = mean(Underpricing))
+final %>% filter(hightech_dummy == 1) %>% summarise(mean = mean(Underpricing))
+
+
+# Model
 M <-cor(final)
+M
 corrplot(M, type="upper", order="hclust",
          col=brewer.pal(n=8, name="RdYlBu"))
 
 Model <- lm(final$Underpricing ~ final$`Gross Proceeds Inc. Overallotment Exercised` + final$age + final$hightech_dummy + final$dummynation + final$dummyCOVID, data = final)
 summary(Model)
+
+
+
+
+
+
+
