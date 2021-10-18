6f3c0f6a669a80569014c1e0cf692839c864d032
yannik207 <ysacherer@icloud.com>
2021-10-18 13:07:02 +0200

    industry check

--- Bachelorarbeit_Skript.R
+++ Bachelorarbeit_Skript.R
@@ -3,13 +3,12 @@
 
 # nötige Packete laden
 library(readxl)
+library(timetk)
 library(writexl)
 library(tidyverse)    
 library(BatchGetSymbols)
 library(plyr)
 library(lubridate)
-library(corrplot)
-library(RColorBrewer)
 library(xlsx)
 library(caret)
 library(MASS)
@@ -97,6 +96,151 @@ data_wo_covid <- orderd_data %>% select(Underpricing, `Issue Date`, `Issuer TRBC
 
 t.test(data_w_covid$Underpricing, data_wo_covid$Underpricing, alternative = "greater")
 
+# was there a change in the industry that went public over the time 
+industry_df <- orderd_data
+
+industry_df$`Issuer TRBC Industry`[industry_df$`Issuer TRBC Industry` %in% hightechsic] <- "hightech"
+industry_df$`Issuer TRBC Industry`[industry_df$`Issuer TRBC Industry` %in% c("Investment Management & Fund Operators", 	
+                                                                           "Investment Trusts",
+                                                                           "Banks",
+                                                                           "Diversified Financial Services",
+                                                                           "Consumer Financial Services",
+                                                                           "Insurance Brokers",
+                                                                           "Life & Health Insurance",
+                                                                           "Real Estate Development & Operations",
+                                                                           "Specialty Investment Services",
+                                                                           "Real Estate Services",
+                                                                           "Financial & Commodity Market Operators",
+                                                                           "Property & Casualty Insurance",
+                                                                           "Multiline Insurance")] <- "Finanzen"
+
+
+industry_df$`Issuer TRBC Industry`[industry_df$`Issuer TRBC Industry` %in% c("Generic & Specialty Pharmaceuticals",
+                                                                             "Advanced Medical Equipment",
+                                                                             "Drug Retailers",
+                                                                             "Biotechnology & Medical Research",
+                                                                             "Healthcare Facilities & Services",
+                                                                             "Medical Equipment, Supplies & Distribution")] <- "Medizin"
+
+industry_df$`Issuer TRBC Industry`[industry_df$`Issuer TRBC Industry` %in% c("Auto Vehicles, Parts & Service Retailers",
+                                                                             "Heavy Machinery & Vehicles",
+                                                                             "Aerospace & Defense",
+                                                                             "Industrial Machinery & Equipment",
+                                                                             "Auto, Truck & Motorcycle Parts",
+                                                                             "Auto & Truck Manufacturers",
+                                                                             "Heavy Electrical Equipment"
+                                                                             )] <- "Maschinen"
+
+industry_df$`Issuer TRBC Industry`[industry_df$`Issuer TRBC Industry` %in% c("Renewable Energy Equipment & Services",
+                                                                             "Oil-related Services and Equipment",
+                                                                             "Oil & Gas Transportation Services",
+                                                                             "Oil & Gas Refining and Marketing",
+                                                                             "Oil & Gas Exploration and Production",
+                                                                             "Coal",
+                                                                             "Oil & Gas Drilling",
+                                                                             "Natural Gas Utilities")] <- "Energie"
+
+industry_df$`Issuer TRBC Industry`[industry_df$`Issuer TRBC Industry` %in% c("Advertising & Marketing",
+                                                                             "Non-Profit/Private Organizations/Services",
+                                                                             "Personal Services",
+                                                                             "Construction & Engineering",
+                                                                             "Environmental Services",
+                                                                             "Passenger Transportation, Ground & Sea",
+                                                                             "Leisure & Recreation",
+                                                                             "Construction Supplies & Fixtures",
+                                                                             "Freight Transportation, Ground",
+                                                                             "Broadcasting",
+                                                                             "Employment Services",
+                                                                             "Business Support Services",
+                                                                             "Business Support Supplies")] <- "Service"
+ 
+industry_df$`Issuer TRBC Industry`[industry_df$`Issuer TRBC Industry` %in% c("Home Furnishings Retailers",
+                                                                             "Other Other Specialty Retailers",
+                                                                             "Home Improvement Products & Services Retailers",
+                                                                             "Department Stores",
+                                                                             "Discount Stores",
+                                                                             "Food Distribution & Convenience Stores",
+                                                                             "Apparel & Accessories Retailers")] <- "Einzelhandel"
+
+industry_df$`Issuer TRBC Industry`[industry_df$`Issuer TRBC Industry` %in% c("Tobacco",
+                                                                             "Home Furnishings",
+                                                                             "Entertainment Production",
+                                                                             "Household Products",
+                                                                             "Paper Products",
+                                                                             "Casinos & Gaming",
+                                                                             "Forest & Wood Products",
+                                                                             "Apparel & Accessories",
+                                                                             "Toys & Games",
+                                                                             "Personal Products")] <- "Konsum"
+ 
+industry_df$`Issuer TRBC Industry`[industry_df$`Issuer TRBC Industry` %in% c("Precious Metals & Minerals",
+                                                                             "Construction Materials",
+                                                                             "Commodity Chemicals",
+                                                                             "Specialty Chemicals",
+                                                                             "Steel",
+                                                                             "Specialty Mining & Metals",
+                                                                             "Non-Paper Containers & Packaging",
+                                                                             "Water & Other Utilities",
+                                                                             "Agricultural Chemicals",
+                                                                             "Semiconductors")] <- "Materialien"
+
+industry_df$`Issuer TRBC Industry`[industry_df$`Issuer TRBC Industry` %in% c("Recreational Products",
+                                                                             "Homebuilding",
+                                                                             "Food Processing",
+                                                                             "Hotels, Motels & Cruise Lines",
+                                                                             "Fishing & Farming",
+                                                                             "Restaurants",
+                                                                             "Airlines")] <- "andere"
+unique(industry_df$`Issuer TRBC Industry`)
+
+industry_2016 <- industry_df %>% filter(`Issue Date` > "2016-01-13", `Issue Date` < "2016-12-30")
+industry_2017 <- industry_df %>% filter(`Issue Date` > "2017-01-01", `Issue Date` < "2017-12-30")
+industry_2018 <- industry_df %>% filter(`Issue Date` > "2018-01-01", `Issue Date` < "2018-12-30")
+industry_2019 <- industry_df %>% filter(`Issue Date` > "2019-01-01", `Issue Date` < "2019-12-30")
+industry_2020 <- industry_df %>% filter(`Issue Date` > "2020-01-01", `Issue Date` < "2020-12-30")
+industry_2021 <- industry_df %>% filter(`Issue Date` > "2016-01-01", `Issue Date` < "2016-12-30")
+
+dustry_counts_2016 <- table(industry_2016$`Issuer TRBC Industry`)
+prop_16 <- dustry_counts_2016 / sum(dustry_counts_2016)
+
+pie(prop_16, main = "2016")
+
+dustry_counts_2017 <- table(industry_2017$`Issuer TRBC Industry`)
+prop_17 <- dustry_counts_2017 / sum(dustry_counts_2017)
+
+pie(prop_17, main = "2017")
+
+dustry_counts_2018 <- table(industry_2018$`Issuer TRBC Industry`)
+prop_18 <- dustry_counts_2018 /sum(dustry_counts_2018)
+
+pie(prop_18, main = "2018")
+
+dustry_counts_2019 <- table(industry_2019$`Issuer TRBC Industry`)
+prop_19 <- dustry_counts_2019 / sum(dustry_counts_2019)
+
+pie(prop_19, main = "2019")
+
+dustry_counts_2020 <- table(industry_2020$`Issuer TRBC Industry`)
+prop_20 <- dustry_counts_2020 / sum(dustry_counts_2020)
+
+pie(prop_20, main = "2020")
+
+dustry_counts_2021 <- table(industry_2021$`Issuer TRBC Industry`)
+prop_21 <- dustry_counts_2021 / sum(dustry_counts_2021)
+pie(prop_21, main = "2021", labels = names(dustry_counts_2021), srt = 10)
+
+
+
+
+#checking for distribution
+round(prop.table(table(industry_df$`Issuer TRBC Industry`, industry_df$dummyCOVID)),2)
+
+#chi squared
+
+chisq.test(industry_df$dummyCOVID, industry_df$`Issuer TRBC Industry`)
+fisher.test(industry_df$dummyCOVID, industry_df$`Issuer TRBC Industry`, simulate.p.value=TRUE)
+
+
 
 
 
@@ -112,24 +256,22 @@ subsample <- na.omit(subsample)
 
 # afterwards i cleaned up the whole dataset and startet at the beginning with new dummyvariables for the orderd dataset, but did most of it in excel
 
-write_xlsx(orderd_data, "/Users/yanniksa/Desktop/Uni/QM2/Bachelorthesis/orderd_data.xlsx")
-write_xlsx(subsample, "/Users/yanniksa/Desktop/Uni/QM2/Bachelorthesis/subsample.xlsx")
-#ordered_data_right <- read_xlsx("orderd_data.xlsx")
+#write_xlsx(orderd_data, "/Users/yanniksa/Desktop/Uni/QM2/Bachelorthesis/orderd_data.xlsx")
+#write_xlsx(subsample, "/Users/yanniksa/Desktop/Uni/QM2/Bachelorthesis/subsample.xlsx")
+ordered_data_right <- read_xlsx("orderd_data.xlsx")
 subsample_right <- read_xlsx("subsample.xlsx")
 
 
 
-chisq.test(ordered_data_right$dummyCOVID, ordered_data_right$`Issuer TRBC Industry`)
-fisher.test(ordered_data_right$dummyCOVID, ordered_data_right$`Issuer TRBC Industry`, simulate.p.value=TRUE)
 
 
 
 # how underpricing evolved over the time
-underpricing_year <- ordered_data_right %>% summarise_by_time(.date_var = `Issue Date`, .by = "year", mean = mean(Underpricing))
+underpricing_year <- ordered_data_right %>% summarise_by_time(.date_var = `Issue Date`, .by = "month", mean = mean(Underpricing))
 
 plot(x = underpricing_year$`Issue Date`, y = underpricing_year$mean, type = "l", main = "IPO Underpricing Zeitreihe", ylab = "Underpricing in %", xlab = "Jahr")
 
-final_right <- ordered_data_right[c(2, 10, 12, 14, 15)]
+final_right <- ordered_data_right[c(2, 10, 12, 13, 14)]
 subsample_right <- subsample_right[c(2, 10, 12, 13, 14, 15)]
 
 # how many companies are in US and Europa
@@ -163,20 +305,9 @@ sum(final_right$dummyCOVID)
 sum(final_right$dummynation)
 
 
-# Model
-M <-cor(final_right)
-round(M,3)
-
-# Model for subsample
-M_age <- cor(subsample_right, method = "spearman")
-round(M_age,3)
-
 names <- c("dummyCOVID", "hightech_dummy", "dummynation")
 final_right[,names] <- lapply(final_right[,names], factor)
 subsample_right[,names] <- lapply(subsample_right[,names], factor)
-
-corrplot(M, type="upper", order="hclust",
-         col=brewer.pal(n=8, name="RdYlBu"))
 
 
 Model <- lm(final_right$Underpricing ~ final_right$IPO_Erlös + final_right$hightech_dummy + final_right$dummynation + final_right$dummyCOVID, data = final_right)
@@ -192,6 +323,23 @@ final_right_905 <- final_right[c(-905, -1093, -1088),]
 Model_905 <- lm(final_right_905$Underpricing ~ final_right_905$IPO_Erlös + final_right_905$hightech_dummy + final_right_905$dummynation + final_right_905$dummyCOVID, data = final_right_905)
 summary(Model_905)
 
+
+# compare usa and eu
+final_right_eu <- final_right_905 %>% filter(dummynation == 1)
+
+final_right_usa <- final_right_905 %>% filter(dummynation == 0)
+
+final_right_eu <- final_right_eu[,-5]
+
+final_right_usa <- final_right_usa[,-5]
+
+Model_eu <- lm(final_right_eu$Underpricing ~ final_right_eu$IPO_Erlös + final_right_eu$dummyCOVID + final_right_eu$hightech_dummy, data = final_right_eu)
+
+summary(Model_eu)
+
+Model_usa <- lm(final_right_usa$Underpricing ~ final_right_usa$IPO_Erlös + final_right_usa$dummyCOVID + final_right_usa$hightech_dummy, data = final_right_usa)
+
+summary(Model_usa)
 
 Model_age <- lm(subsample_right$Underpricing ~ subsample_right$IPO_Erlös + subsample_right$dummyCOVID + subsample_right$age + subsample_right$hightech_dummy + subsample_right$dummynation, data = subsample_right)
 summary(Model_age)
